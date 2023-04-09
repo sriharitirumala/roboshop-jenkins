57dc40cc18a850800a220fdfbbb283858bbea2b6
sriharitirumala <sritirumala30@gmail.com>
2023-04-09 15:11:28 +0530

    adding vpc subnets, routes, route table associations

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# Configure the Jenkins Provider
+provider "jenkins" {
+  server_url = "http://172.31.3.167:8080"
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+}
+
+data "aws_ssm_parameter" "jenkins_user"{
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass"{
+  name = "jenkins.pass"
+}
