1e935a0d2d68f3b49860bf8b883946aa4417e730
sriharitirumala <sritirumala30@gmail.com>
2023-04-10 11:15:20 +0530

    adding vpc subnets, routes, route table associations

--- vars.tf
+++ vars.tf
@@ -1,9 +1,16 @@
 variable "folders" {
-  default = ["Infra"]
+  default = ["Infra", "CI-Pipeline"]
 }
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-infra" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" }
+
   ]
 }
