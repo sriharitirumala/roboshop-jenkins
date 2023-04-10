7bc7885be277ad4ddbb732cc4b157608bee0e2c5
sriharitirumala <sritirumala30@gmail.com>
2023-04-10 11:56:03 +0530

    adding vpc subnets, routes, route table associations

--- vars.tf
+++ vars.tf
@@ -5,12 +5,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-infra" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/frontend" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/catalogue" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/cart" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sriharitirumala/payment" }
 
   ]
 }
