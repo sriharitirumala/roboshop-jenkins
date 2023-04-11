3a81fdeec7fad9335d563f42aeb10587a8094450
sriharitirumala <sritirumala30@gmail.com>
2023-04-11 17:47:39 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -11,12 +11,13 @@ resource "jenkins_job" "job" {
 
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.jobs, count.index), "name", null)
   })
 
-  lifecycle {
-    ignore_changes = [ template ]
-
-  }
+#  lifecycle {
+#    ignore_changes = [ template ]
+#
+#  }
 }
 
 data "aws_instance" "jenkins"{
